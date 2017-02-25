bf048b3a6d1f703515d216dfc809821ee2cea4e5
Cory Reed <swashcap@gmail.com>
2017-02-24 23:00:08 -0800

    Add address controller unit test.

--- test/controllers/address.js
+++ test/controllers/address.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const address = require('../../server/controllers/address.js');
-const mockery = require('mockery');
 const sinon = require('sinon');
 const tape = require('tape');
+
+const address = require('../../server/controllers/address.js');
+const makeRequest = require('../../server/utils/make-request.js');
 const reverseGeocodingStub = require('../stubs/reverse-geocoding.json');
 
 tape('address :: get', (t) => {
-  const makeRequestStub = sinon.stub()
+  const makeRequestStub = sinon.stub(makeRequest, 'get')
     .returns(Promise.resolve(reverseGeocodingStub));
   const request = {
     params: {
@@ -21,20 +22,27 @@ tape('address :: get', (t) => {
   };
   const replySpy = sinon.spy();
 
-  mockery.registerMock(
-    '../../server/utils/make-request.js',
-    makeRequestStub
-  );
-  const teardown = () => mockery.deregisterMock('../../server/utils/make-request.js');
+  t.plan(2);
 
   address.get(request, replySpy)
     .then((response) => {
-      if (response.isBoom) {
-        console.log(makeRequestStub.called);
-        debugger;
+      if (response && response.isBoom) {
         throw response;
       }
+
+      t.equal(replySpy.callCount, 1, 'calls reply');
+      t.deepEqual(
+        replySpy.firstCall.args[0],
+        {
+          administrativeAreaLevel1:
+            reverseGeocodingStub.results[0].address_components[5].long_name,
+          sublocality:
+            reverseGeocodingStub.results[0].address_components[3].long_name,
+        },
+        'modifies geojson API response'
+      );
     })
     .catch(t.end)
-    .then(teardown, teardown);
+    .then(makeRequestStub.restore, makeRequestStub.restore);
 });
+
--- test/index.js
+++ test/index.js
@@ -1,14 +1,4 @@
 'use strict';
 
-const mockery = require('mockery');
-
-
-mockery.enable({
-  warnOnUnregistered: false,
-  // useCleanCache: true,
-});
-
 require('./controllers/address.js');
-
-mockery.disable();
 
